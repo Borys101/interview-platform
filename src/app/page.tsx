0d91c90d0c2fc26ee056a8a8465cfa490f763cb9
Borys <boris.ivlev.11@gmail.com>
2025-03-17 19:15:45 +0100

    Add navbar component and fix some bugs in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
-    return (
-        <div className="m-10">
-            <SignInButton>
-                <Button>Login</Button>
-            </SignInButton>
-        </div>
-    );
+    return <div className="m-10">Home Page</div>;
 }
